0c4ff7a64f96f6d1cbceadd41cacf875789d1a18
Adam Mathieson <mathieson1222@gmail.com>
2019-03-01 08:30:41 -0800

    project setup and data array brought in through axios

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -9,7 +10,27 @@ class App extends Component {
     super(props);
     this.state = {
       smurfs: [],
+      error: ''
     };
+  }
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => {
+      this.setState({ smurfs: res.data })
+    })
+    .catch(err => {
+      this.setState ({ error: err})
+    })
+  }
+
+  addSmurf = (newSmurf) => {
+    axios
+      .post('http://localhost:3333/smurfs', newSmurf)
+      .then(res => {
+        this.setState({ smurfs: res.data})
+      })
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
@@ -17,7 +38,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm addSmurf={this.addSmurf} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
